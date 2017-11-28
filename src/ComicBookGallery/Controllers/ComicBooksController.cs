76b7d6ec9dfabb639caa6701e400e86932a4403c
Michael Lacey <laceymwes@gmail.com>
2017-11-28 05:02:45 -0700

    Updated comic books controller to use repository

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -4,30 +4,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ComicBookGallery.Data;
 
 namespace ComicBookGallery.Controllers
 {
      public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
-        {
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHTML = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist() { Name = "Dan Slott", Role = "Script"},
-                    new Artist() { Name = "Humerto Ramos", Role = "Pencils"},
-                    new Artist() { Name = "Victor Olazaba", Role = "Colors"},
-                    new Artist() { Name = "Edgar Delgado", Role = "Colors"},
-                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters"}
-                }
-            };
+        private ComicBookRepository _comicBookRepository = null;
 
+        public ComicBooksController()
+        {
+            _comicBookRepository = new ComicBookRepository();
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if(id == null)
+            {
+                return HttpNotFound();
+            }
+
+            // cast id to int, since int parameter is nullable
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
             return View(comicBook);
-           
         }
            
     }
--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -12,13 +12,14 @@ namespace ComicBookGallery.Data
         {
             new ComicBook()
             {
+                Id = 1,
                 SeriesTitle = "The Amazing Spider-Man",
                 IssueNumber = 700,
                 DescriptionHTML = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
                 Artists = new Artist[]
                {
                     new Artist() { Name = "Dan Slott", Role = "Script"},
-                    new Artist() { Name = "Humerto Ramos", Role = "Pencils"},
+                    new Artist() { Name = "Humberto Ramos", Role = "Pencils"},
                     new Artist() { Name = "Victor Olazaba", Role = "Colors"},
                     new Artist() { Name = "Edgar Delgado", Role = "Colors"},
                     new Artist() { Name = "Chris Eliopoulos", Role = "Letters"},
@@ -27,6 +28,7 @@ namespace ComicBookGallery.Data
             },
              new ComicBook()
              {
+                Id = 2,
                 SeriesTitle = "The Amazing Spider-Man",
                 IssueNumber = 657,
                 DescriptionHTML = "<p><strong>FF: THREE TIE-IN.</strong> Spider-Man visits the FF for a very private wake--just for family.</p>",
@@ -42,6 +44,7 @@ namespace ComicBookGallery.Data
              },
              new ComicBook()
              {
+                Id = 3,
                 SeriesTitle = "Bone",
                 IssueNumber = 50,
                 DescriptionHTML = "<p><strong>The Dungeon & The Parapet, Part 1.</strong> Thorn is discovered by Lord Tarsil and the corrupted Stickeaters and thrown into a dungeon with Fone Bone. As she sleeps, a message comes to her about the mysterious \"Crown of Horns\".</p>",
